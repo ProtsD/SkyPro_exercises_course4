e03e2f9256c57b4245db8966905d6661665000fe
Denis <protsdv@gmail.com>
2023-04-28 11:45:19 +0300

    Initial commit

---
+++ src/main/java/course4/hw1_spring/skypro_exercises_course4/SkyProExercisesCourse4Application.java
@@ -0,0 +1,13 @@
+package course4.hw1_spring.skypro_exercises_course4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProExercisesCourse4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProExercisesCourse4Application.class, args);
+    }
+
+}
---
+++ src/test/java/course4/hw1_spring/skypro_exercises_course4/SkyProExercisesCourse4ApplicationTests.java
@@ -0,0 +1,13 @@
+package course4.hw1_spring.skypro_exercises_course4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProExercisesCourse4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
